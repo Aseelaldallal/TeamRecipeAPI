103264f452194eb9e0d245bff6b891d6b69cdda0
aseelaldallal <aseelaldallal@gmail.com>
2018-11-03 13:43:33 -0400

    tslint

--- lib/app.ts
+++ lib/app.ts
@@ -1,41 +1,37 @@
-import * as express from "express";
-import * as bodyParser from "body-parser";
-import { Request, Response } from "express";
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Request, Response } from 'express';
 
 class App {
+	public app: express.Application;
+	constructor() {
+		this.app = express();
+		this.config();
+		this.routes();
+	}
 
-  constructor() {
-    this.app = express();
-    this.config();
-    this.routes();
-  }
+	private config(): void {
+		this.app.use(bodyParser.json());
+		this.app.use(bodyParser.urlencoded({ extended: false }));
+	}
 
-  public app: express.Application;
+	private routes(): void {
+		const router = express.Router();
 
-  private config(): void {
-    this.app.use(bodyParser.json());
-    this.app.use(bodyParser.urlencoded({ extended: false }));
-  }
+		router.get('/', (req: Request, res: Response) => {
+			res.status(200).send({
+				message: 'Hello World!'
+			});
+		});
 
-  private routes(): void {
-    const router = express.Router();
+		router.post('/', (req: Request, res: Response) => {
+			const data = req.body;
+			// query a database and save data
+			res.status(200).send(data);
+		});
 
-    router.get('/', (req: Request, res: Response) => {
-      res.status(200).send({
-        message: 'Hello World!'
-      })
-    });
-
-    router.post('/', (req: Request, res: Response) => {
-      const data = req.body;
-      // query a database and save data
-      res.status(200).send(data);
-    });
-
-    this.app.use('/', router)
-
-  }
-
+		this.app.use('/', router);
+	}
 }
 
 export default new App().app;
