2f54d1cc20a8a66ecedf630afc10b7fcb57748d3
aseelaldallal <aseelaldallal@gmail.com>
2018-11-04 14:14:21 -0500

    add error handling and routes

--- lib/src/app.ts
+++ lib/src/app.ts
@@ -2,15 +2,18 @@ import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as mongoose from 'mongoose';
 import { dbURL } from '../config/database';
-import { User } from './models/user';
+
+import { router as teamRouter } from './routes/teams';
 
 class App {
 	public app: express.Application;
+
 	constructor() {
 		this.app = express();
 		this.connectToDB();
 		this.config();
 		this.routes();
+		this.handleErrors();
 	}
 
 	private connectToDB = () => {
@@ -27,20 +30,19 @@ class App {
 
 	private routes(): void {
 		const router = express.Router();
-
-		router.get('/', (req: express.Request, res: express.Response) => {
-			res.status(200).send({
-				message: 'Hello World!'
-			});
-		});
-
-		router.post('/', (req: express.Request, res: express.Response) => {
-			const data = req.body;
-			// query a database and save data
-			res.status(200).send(data);
-		});
-
+		this.app.use('/teams', teamRouter);
 		this.app.use('/', router);
+	}
+
+	private handleErrors() {
+		// Expand this later
+		this.app.use((err, req, res, next) => {
+			if (err.message === 'access denied') {
+				res.status(403);
+				res.json({ error: err.message });
+			}
+			next(err);
+		});
 	}
 }
 
