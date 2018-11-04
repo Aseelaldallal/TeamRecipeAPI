97ed014b1330d7c706964af4fabdf3e4dcb03890
aseelaldallal <aseelaldallal@gmail.com>
2018-11-04 17:50:27 -0500

    backbone, need to add passport first

---
+++ lib/src/routes/recipe.ts
@@ -0,0 +1,46 @@
+/* ------------------------------------- */
+/* -----------------SETUP--------------- */
+/* ------------------------------------- */
+
+import * as express from 'express';
+import 'express-async-errors';
+import { IIngredient, IRecipe } from '../interfaces/recipe';
+import { Recipe } from '../models/recipe';
+
+export const router = express.Router();
+
+/* ------------------------------------- */
+/* --------------INDEX ROUTE------------ */
+/* ------------------------------------- */
+
+router.get('/', async (req: express.Request, res: express.Response) => {
+	const recipes = await Recipe.find();
+	res.json(recipes);
+});
+
+/* ------------------------------------- */
+/* -------------CREATE ROUTE------------ */
+/* ------------------------------------- */
+
+router.post('/new', async (req: express.Request, res: express.Response) => {});
+
+/* ------------------------------------- */
+/* ---------------SHOW ROUTE------------ */
+/* ------------------------------------- */
+
+router.get('/:id', async (req: express.Request, res: express.Response) => {});
+
+/* ------------------------------------- */
+/* --------------UPDATE ROUTE----------- */
+/* ------------------------------------- */
+
+router.put('/:id', async (req: express.Request, res: express.Response) => {});
+
+/* ------------------------------------- */
+/* -------------DESTROY ROUTE----------- */
+/* ------------------------------------- */
+
+router.delete(
+	'/:id',
+	async (req: express.Request, res: express.Response) => {}
+);
