cbd99c81d6f4c85d68812ad229ecfce0ef74b67d
Aseelaldallal <aseelaldallal@gmail.com>
2019-07-04 12:17:44 -0400

    tests

--- lib/tests/routes/team.spec.ts
+++ lib/tests/routes/team.spec.ts
@@ -1,7 +1,44 @@
+import app from '../../src/app';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+import { User, IUser } from '../../src/models/user';
+import { Test } from '../TestInitializer';
 
+const expect = chai.expect;
+chai.use(chaiHttp);
 
+before(async () => {
+	await Test.start();
+});
 
-// Tests
+after(() => {
+	Test.end();
+});
+
+describe('POST /new', () => {
+	let adminUser: IUser;
+	let jwtToken: string;
+
+	before(async () => {
+		adminUser = User.createDummy();
+		const response = await chai
+			.request(app)
+			.post('/register')
+			.type('form')
+			.send(adminUser);
+		jwtToken = response.body.token;
+	});
+
+	it('Should allow an authenticated user to create a new team', async () => {
+		const response = await chai
+			.request(app)
+			.post('/team/new')
+			.set('Authorization', `Bearer ${jwtToken}`)
+			.type('form')
+			.send({ name: 'BubbleGum Team' });
+		expect(response.body.name).to.equal('BubbleGum Team');
+	});
+});
 
 // CREATE
 // Unauthenticated users cannot create team
@@ -14,4 +51,4 @@
 // New admin must be a member
 // Switching admin replaces new admin, moves him to members, removes new admin from members
 // Changing team members only changes team members only
-// Changing 
+// Changing
